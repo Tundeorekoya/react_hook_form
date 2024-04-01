fcfcb5fc40e285a5e7803d9f8dc8da4ff72d651e
tundeorekoya <tundeorekoya1998@gmail.com>
2024-04-01 05:25:48 -0700

    update

--- src/components/youtubeform.tsx
+++ src/components/youtubeform.tsx
@@ -9,22 +9,62 @@ type FormValue = {
 
 const youtubeform = () => {
   const form = useForm<FormValue>();
-  const { register, control, handleSubmit } = form;
+  const { register, control, handleSubmit, formState } = form;
+  const { errors } = formState;
   const onSubmit = (data: FormValue) => {
     console.log("form submitted", data);
   };
   return (
-    <div>
+    <div className="form-control">
       <h1>WELCOME</h1>
-      <form onSubmit={handleSubmit(onSubmit)}>
+      <form onSubmit={handleSubmit(onSubmit)} noValidate>
         <label htmlFor="username">Username</label>
-        <input type="text" id="username" {...register("username")} />
+        <input
+          type="text"
+          id="username"
+          {...register("username", {
+            required: {
+              value: true,
+              message: "username is required",
+            },
+          })}
+        />
+        <p className="error">{errors.username?.message}</p>
 
         <label htmlFor="email">E-mail</label>
-        <input type="email" id="email" {...register("email")} />
+        <input
+          type="email"
+          id="email"
+          {...register("email", {
+            pattern: {
+              value:
+                /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+              message: "invalid email format",
+            },
+            validate: {
+              notAdmin: (fieldValue) => {
+                return (
+                  fieldValue !== "admin@example.com" ||
+                  "Enter a Different Email"
+                );
+              },
+            },
+          })}
+        />
+        <p className="error">{errors.email?.message}</p>
 
         <label htmlFor="channel">Channel</label>
-        <input type="text" id="channel" {...register("channel")} />
+        <input
+          type="text"
+          id="channel"
+          {...register("channel", {
+            required: {
+              value: true,
+              message: "channel is required",
+            },
+          })}
+        />
+        <p className="error">{errors.channel?.message}</p>
 
         <button>Submit</button>
       </form>
